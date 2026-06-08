03c581f6771a12cf5d0e14a1731d8d37ae0f1484
ObaShion <obashion09@icloud.com>
2026-06-09 00:07:19 +0900

    Initial Commit

---
+++ KiraKira/KiraKiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiraKiraApp.swift
+//  KiraKira
+//
+//  Created by 大場史温 on 2026/06/09.
+//
+
+import SwiftUI
+
+@main
+struct KiraKiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
